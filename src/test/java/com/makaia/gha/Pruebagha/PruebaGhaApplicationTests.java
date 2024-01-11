97cc2eef420a238e679e500e8e29fe4af800a64c
ABEL-pixel-nube <labelardo.477@gmail.com>
2024-01-11 14:54:43 -0500

    first commit

---
+++ src/test/java/com/makaia/gha/Pruebagha/PruebaGhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makaia.gha.Pruebagha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
